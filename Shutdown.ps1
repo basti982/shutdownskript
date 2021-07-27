93cc3efb857359ae148e4d2adb8072c90303e9f7
TECHNIKER MEYER <45665545+basti982@users.noreply.github.com>
2021-07-27 15:30:12 +0200

    Add files via upload

---
+++ Shutdown.ps1
@@ -0,0 +1,6 @@
+﻿$password = "Secret" | ConvertTo-SecureString -asPlainText -Force
+$username = "mydomain\administrator"
+$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+Import-Csv c:\Computers.csv | ForEach-Object {
+    	Invoke-Command -ComputerName $($_.Name) {shutdown -f -s} -Credential $credential
+}
